e3047a89e8e0b22bb6f3ff0f4e3889b2a92b3787
Sebastian Kroczek <sk@xbug.de>
2016-07-28 16:30:48 +0200

    Adds JMS\ObjectRouting\Symfony\Symfony22Adapter as an adapter for symfony/routing >=2.2

---
+++ tests/JMS/Tests/ObjectRouting/Symfony/Symfony22AdapterTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JMS\Tests\ObjectRouting\Symfony;
+
+use JMS\ObjectRouting\Symfony\Symfony22Adapter;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class Symfony22AdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Symfony22Adapter */
+    private $adapter;
+    private $router;
+
+    public function testGenerate()
+    {
+        $this->router->expects($this->once())
+            ->method('generate')
+            ->with('foo', array('bar' => 'baz'), UrlGeneratorInterface::ABSOLUTE_URL)
+            ->will($this->returnValue('/foo-bar-baz'));
+
+        $this->assertEquals('/foo-bar-baz', $this->adapter->generate('foo', array('bar' => 'baz'), true));
+    }
+
+    protected function setUp()
+    {
+        $this->adapter = new Symfony22Adapter(
+            $this->router = $this->getMock('Symfony\Component\Routing\RouterInterface')
+        );
+    }
+}
